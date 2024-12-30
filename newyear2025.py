707d7cf5f60799bb57d9ab8d15a87a7bde3f4bc5
Akshat <101553500+Goldisoni@users.noreply.github.com>
2024-12-30 23:59:58 +0530

    Add files via upload
    python voice code for happy new year 2025

---
+++ newyear2025.py
@@ -0,0 +1,15 @@
+import pyttsx3
+import socketserver
+
+engine = pyttsx3.init()
+
+engine.say("Happy New Year 2025! \
+           Wishing you a fantastic year ahead!")
+
+engine.runAndWait()
+
+#for printing message on console
+
+print("Happy New Year 2025!\
+       Wishing you a fantastic year ahead!")
+
